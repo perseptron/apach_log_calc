d8c67a553a15c0a9c4c00eba706f0fee114c4c5a
perseptron <30828895+perseptron@users.noreply.github.com>
2023-07-19 22:35:47 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #get Apache log file as a parameter
-DEFAULT_FILE=apache_logs
-log_file=${1:-$DEFAULT_FILE}
+log_file=$1
 
 #declare associative array where ip and bytes downloaded will be stored
 declare -A  stat
@@ -9,32 +8,72 @@ declare -A  stat
 #declare total bytes variable
 total=0
 
+#convert data size to human readable format
 function to_hr() {
-  local int_part="${1%.}"
-  local iter="$2"
+    local int_part=${1%.*}
+    pow=${2:-0}
+    if [[ ${#int_part} -gt 3 ]]; then
+        pow=$(( pow+1 ))
+        val=$(echo "scale=1; $1 / 1024" | bc)
+        to_hr $val $pow
+    else
+        case "$pow" in
+           0)
+             echo "$1"B
+             ;;
+           1)
+             echo "$1"KiB
+             ;;
+           2)
+             echo "$1"MiB
+             ;;
+           3)
+             echo "$1"GiB
+             ;;
+           4)
+             echo "$1"TiB
+             ;;
+           5)
+             echo "$1"PiB
+             ;;
+           6)
+             echo "$1"EiB
+             ;;
+           *)
+             echo "too much"
+             ;;
+        esac
+    fi
 }
-
-
 #loop through lines and make calculations
-while read -r line; do
-      split_line=($line)
+while read -ra split_line; do
       #sqip empty lines
       if [[ ${#split_line} -eq 0 ]]; then
-	 continue
+         continue
       fi
       ip=${split_line[0]}
+      #skip wrong IP
+      #TODO re
+      if [[ ${#ip} -le 6 ]]; then
+         continue
+      fi
       traffic=${split_line[9]}
       if [[ ${traffic} == "-" ||  ${traffic} == " " ]]; then
          traffic=0
+      fi
+      #skip wrong size
+      if ! [[ $traffic =~ ^[0-9]+$ ]] ; then
+         echo "bad traffic size: $traffic"
+         continue
       fi
       traff_inc=${stat[$ip]}
       stat[$ip]=$((traff_inc+traffic))
       total=$((total+traffic))
 done <<< $( cat $log_file )
-
+#output
 echo "There are ${#stat[@]} unique ips"
-echo "Total downloaded: $total ($(($total/1024/1024))MiB)"
+totalhr=$(to_hr $total)
+echo "Total downloaded: $total ($totalhr)"
 for ip in "${!stat[@]}"; do
    printf '%-15s %9d\n' "$ip" "${stat[$ip]}"
 done | sort -rn -k2 -k1
-
