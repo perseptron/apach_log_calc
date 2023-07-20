0736b5c33d8aee9bb2156d024ab1bc18a7f17410
perseptron <30828895+perseptron@users.noreply.github.com>
2023-07-20 19:18:17 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,49 +2,15 @@
 #get Apache log file as a parameter
 log_file=$1
 
+# use human readable byte converter
+source ./b_conv.sh
+
 #declare associative array where ip and bytes downloaded will be stored
 declare -A  stat
 
 #declare total bytes variable
 total=0
 
-#convert data size to human readable format
-function to_hr() {
-    local int_part=${1%.*}
-    pow=${2:-0}
-    if [[ ${#int_part} -gt 3 ]]; then
-        pow=$(( pow+1 ))
-        val=$(echo "scale=1; $1 / 1024" | bc)
-        to_hr $val $pow
-    else
-        case "$pow" in
-           0)
-             echo "$1"B
-             ;;
-           1)
-             echo "$1"KiB
-             ;;
-           2)
-             echo "$1"MiB
-             ;;
-           3)
-             echo "$1"GiB
-             ;;
-           4)
-             echo "$1"TiB
-             ;;
-           5)
-             echo "$1"PiB
-             ;;
-           6)
-             echo "$1"EiB
-             ;;
-           *)
-             echo "too much"
-             ;;
-        esac
-    fi
-}
 #loop through lines and make calculations
 while read -ra split_line; do
       #sqip empty lines
