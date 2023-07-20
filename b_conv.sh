e66b14423f79d96856b0d8af986ae83f5eccd8e6
perseptron <30828895+perseptron@users.noreply.github.com>
2023-07-20 19:16:21 +0300

    Update b_conv.sh

--- b_conv.sh
+++ b_conv.sh
@@ -1,34 +1,21 @@
-#!/bin/bash
-
+#convert data size to human readable format
 function to_hr() {
     local int_part=${1%.*}
     pow=${2:-0}
     if [[ ${#int_part} -gt 3 ]]; then
-	pow=$(( pow+1 ))
-	val=$(printf "%.1f" $(echo "scale=10; $1 / 1024" | bc))
-	to_hr $val $pow
+        pow=$(( pow+1 ))
+        val=$(echo "scale=1; $1 / 1024" | bc)
+        to_hr $val $pow
     else
-	case "$pow" in
-	   0)
-	     echo $1B
-	     ;;
-	   1)
-	     echo $1KiB
-	     ;;
-	   2)
-	     echo $1MiB
-	     ;;
-	   3)
-	     echo $1Gib
-	     ;;
-	   4)
-	     echo $1TiB
-	     ;;
-	   *)
-	     echo "too much"
-	     ;;
-	esac
+        case "$pow" in
+           0) echo "$1"B ;;
+           1) echo "$1"KiB ;;
+           2) echo "$1"MiB ;;
+           3) echo "$1"GiB ;;
+           4) echo "$1"TiB ;;
+           5) echo "$1"PiB ;;
+           6) echo "$1"EiB ;;
+           *) echo "too much" ;;
+        esac
     fi
 }
-
-to_hr $1
